23f34d9d5bb8c9a9bad7945e82ddcb6e44a6f0f0
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2022-06-03 19:11:05 +0300

    FarmingPlus 1.1

--- FarmingPlus.php
+++ FarmingPlus.php
@@ -3,7 +3,7 @@
 __PocketMine Plugin__
 name=FarmingPlus
 description=Useful plugin for Farming using Hoe
-version=1.0
+version=1.1
 author=ArkQuark
 class=FarmingPlus
 apiversion=11,12
@@ -27,66 +27,59 @@ class FarmingPlus implements Plugin{
 
 				$player = $data["player"];
 				$target = $data["target"];
+				$targetID = $target->getID();
+				$targetMeta = $target->getMetadata();
+				$targetReflection = new ReflectionClass('Block');
+				$targetReflectionMeta = $targetReflection->getProperty('meta');
+				$targetReflectionMeta->setAccessible(true);
+				
 				$itemHeld = $player->getSlot($player->slot);
+				
 				$dropPos = new Position($target->x+0.5, $target->y, $target->z+0.5, $target->level);
 				$pos = new Vector3($target->x, $target->y, $target->z, $target->level);
-
-				if($target->getID() == 59 and $target->getMetadata() == 7 and $itemHeld->isHoe() == true){//Wheat
-					$block = BlockAPI::get(59, 0);
-					if($target->getSide(0)->getID() !== 60) break;
-					else{
-						$target->level->setBlock($pos, $block);
+				
+				if($itemHeld->isHoe() != true) break;
+				if($targetMeta != 7) break;
+				if($target->getSide(0)->getID() != 60) break;
+				
+				switch($targetID){
+					case 59: //wheat
+						$targetReflectionMeta->setValue($target, 0);
+						$target->level->setBlock($target, $target);
+						
 						$item = $this->api->block->fromString("WHEAT_SEEDS");
+						for($i = mt_rand(0, 3); $i > 0; $i--) $this->api->entity->drop($dropPos, $item);
 						
-						for($i = mt_rand(0, 3); $i > 0; $i--){
-							$this->api->entity->drop($dropPos, $item);
-						}
-						$this->api->entity->drop($dropPos, $this->api->block->fromString("WHEAT"));
-					}
-					break;
-				}
-				
-				elseif($target->getID() == 244 and $target->getMetadata() == 7 and $itemHeld->isHoe() == true){//Beetroot
-					$block = BlockAPI::get(244, 0);
-					if($target->getSide(0)->getID() !== 60) break;
-					else{
-						$target->level->setBlock($pos, $block);
+						$item = $this->api->block->fromString("WHEAT");
+						$this->api->entity->drop($dropPos, $item);
+						break;
+						
+					case 244: //beetroot
+						$targetReflectionMeta->setValue($target, 0);
+						$target->level->setBlock($target, $target);
+						
 						$item = $this->api->block->fromString("BEETROOT_SEEDS");
+						for($i = mt_rand(0, 3); $i > 0; $i--) $this->api->entity->drop($dropPos, $item);
+					
+						$item = $this->api->block->fromString("BEETROOT");
+						$this->api->entity->drop($dropPos, $item);
+						break;
 						
-						for($i = mt_rand(0, 3); $i > 0; $i--){
-							$this->api->entity->drop($dropPos, $item);
-						}
-						$this->api->entity->drop($dropPos, $this->api->block->fromString("BEETROOT"));
-					}
-					break;
-				}
-				
-				elseif($target->getID() == 141 and $target->getMetadata() == 7 and $itemHeld->isHoe() == true){//Carrot
-					$block = BlockAPI::get(141, 0);
-					if($target->getSide(0)->getID() !== 60) break;
-					else{
-						$target->level->setBlock($pos, $block);
+					case 141: //carrot
+						$targetReflectionMeta->setValue($target, 0);
+						$target->level->setBlock($target, $target);
+						
 						$item = $this->api->block->fromString("CARROT");
+						for($i = mt_rand(0, 2); $i > 0; $i--) $this->api->entity->drop($dropPos, $item);
+						break;
 						
-						for($i = mt_rand(0, 2); $i > 0; $i--){
-							$this->api->entity->drop($dropPos, $item);
-						}
-					}
-					break;
-				}
-				
-				elseif($target->getID() == 142 and $target->getMetadata() == 7 and $itemHeld->isHoe() == true){//Potato
-					$block = BlockAPI::get(142, 0);
-					if($target->getSide(0)->getID() !== 60) break;
-					else{
-						$target->level->setBlock($pos, $block);
+					case 142: //potato
+						$targetReflectionMeta->setValue($target, 0);
+						$target->level->setBlock($target, $target);
+						
 						$item = $this->api->block->fromString("POTATO");
-						
-						for($i = mt_rand(0, 2); $i > 0; $i--){
-							$this->api->entity->drop($dropPos, $item);
-						}
-					}
-					break;
+						for($i = mt_rand(0, 2); $i > 0; $i--) $this->api->entity->drop($dropPos, $item);
+						break;
 				}
 		}
 	}
