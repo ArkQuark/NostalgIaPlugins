59260476e41ad74ba6557a923354aaf842b44438
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2022-10-10 00:07:48 +0300

    Update GrassPlant.php

--- GrassPlant.php
+++ GrassPlant.php
@@ -4,29 +4,25 @@
 __PocketMine Plugin__
 name=GrassPlant
 description=Replant grass using wheat seeds.
-version=1.4
+version=1.4.1
 author=onlypuppy7
 class=Grass
-apiversion=12
+apiversion=12.1
 */
 
 class Grass implements Plugin{
-	private $api;
 
 	public function __construct(ServerAPI $api, $server = false){
 		$this->api = $api;
 	}
 
 	public function init(){
-		$this->api->addHandler("player.block.touch", array($this, "eventHandle"), 50);
+		$this->api->addHandler("player.block.touch", array($this, "eventHandle"), 5);
 	}
 
 	public function eventHandle($data, $event){
-
-		switch ($event){
-
+		switch($event){
 			case "player.block.touch":
-
 				$player = $data["player"];
 				$target = $data["target"];
 				$tile = $this->api->tile->get(new Position($target->x, $target->y, $target->z, $target->level));
@@ -34,23 +30,21 @@ class Grass implements Plugin{
 				$item = $itemheld->getID();
 
 				if(($target->getID() == 3) and (($item == 295) or ($item == 361) or ($item == 362) or ($item == 458))){
-					$block = BlockAPI::get(2, 0);
-					$pos = new Vector3($target->x,$target->y,$target->z);
+					$block = BlockAPI::get(DIRT);
+					$pos = new Vector3($target->x, $target->y, $target->z);
 					if($target->getSide(1)->isTransparent === false){
 						break;
 					}
 					else{
-					$target->level->setBlock($pos, $block);
-					if($player->getGamemode() == "survival"){
-						$player->removeItem($item, 0, 1);
-					}
+						$target->level->setBlock($pos, $block);
+						if($player->getGamemode() == "survival"){
+							$player->removeItem($item, 0, 1);
+						}
 					}
 				}
 				break;
 		}
 	}
 
-
-	public function __destruct(){
-	}
+	public function __destruct(){}
 }
