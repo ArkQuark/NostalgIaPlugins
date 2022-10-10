d4652273e2869f7fd5d620684c8ab3947a680502
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2022-10-10 14:23:45 +0300

    Update LuckyRace.php

--- LuckyRace.php
+++ LuckyRace.php
@@ -3,8 +3,8 @@
 /*
 __PocketMine Plugin__
 name=LuckyRace
-description=Works only with 2 players!
-version=1.0.2
+description=Plugin for LuckyRace
+version=1.1.0
 author=ArkQuark 
 class=LRmain
 apiversion=12.1
@@ -17,6 +17,7 @@ class LRmain implements Plugin{
 		$this->api = $api;
 		$this->status = "play";
 		$this->prefix = "[LuckyRace] ";
+		$this->players = [];
     }
 
     public function init(){
@@ -24,15 +25,17 @@ class LRmain implements Plugin{
 		if(!file_exists($path)){
 			mkdir($path, 0777);
 		}
-		$this->config = new Config($path."AIO.yml", CONFIG_YAML, [
+		$this->configfile = new Config($path."AIO.yml", CONFIG_YAML, [
 			"info" => "All in one config",
 			"LuckyRace" => [
 				"x" => 127.5,
 				"y" => 70,
 				"z" => 176.5,
-				"level" => "race"
+				"level" => "race",
+				"prize" => DIAMOND_BLOCK
 			]
 		]);
+		$this->config = $this->configfile->get("LuckyRace");
 		
 		$this->api->addHandler("entity.health.change", [$this, "eventHandler"]);
 		$this->api->addHandler("player.block.touch", [$this, "eventHandler"]);
@@ -52,37 +55,36 @@ class LRmain implements Plugin{
 				if($data['target']->getID() != WALL_SIGN) break;
 				$tile = $this->api->tile->get(new Position($data['target']->x, $data['target']->y, $data['target']->z, $data['target']->level));
 				if($tile === false) break;
-				$data = $tile->getText();
-				if($data[0] === "tp @a Arena"){
+				$dataSign = $tile->getText();
+				if($dataSign[0] === "tp @a Arena"){
 					$this->status = "invincible";
+					$data['player']->addItem($this->config["prize"], 0, 1);
 					$players = $this->api->player->getAll();
-					$level = $this->api->level->get($this->config->get("LuckyRace")["level"]);
+					$level = $this->api->level->get($this->config["level"]);
+					$this->api->schedule(10*20, [$this, "pvp"], [], false);
+					$this->api->chat->broadcast($this->prefix."Битва начнется через 10 секунд..\n".$this->prefix."Разбежитесь по углам!");
 					foreach($players as $p){
-						$p->teleport(new Position($this->config->get("LuckyRace")["x"], $this->config->get("LuckyRace")["y"], $this->config->get("LuckyRace")["z"], $level));
-						$p->sendChat($this->prefix."Битва начнется через 10 секунд..\n".$this->prefix."Разбежитесь по углам!");
-						$this->api->schedule(10*20, [$this, "pvp"], [$p], false);
+						$p->teleport(new Position($this->config["x"], $this->config["y"], $this->config["z"], $level));
 					}
 				}
 				break;
 			case "player.death":
-				if($data["cause"] == "void" and $this->status == "pvp"){
-					$this->api->chat->broadcast($this->prefix."Сумо не предусматривается...");
+				if($this->status == "pvp"){
+					unset($this->players[$data['player']->username]);
+					$this->api->chat->broadcast($this->prefix.array_shift($this->players)." выграл эту лаки гонку!");
 					$this->status = "play";
 				}
-				if(is_numeric($data["cause"])){
-					$e = $this->api->entity->get($data["cause"]);
-					if($e->class == ENTITY_PLAYER and $this->status == "pvp"){
-						$this->api->chat->broadcast($this->prefix.$e->player->username." выграл эту лаки гонку!");
-					}
-					$this->status = "play";
-				}
-				
 				break;
 		}
 	}
 	
-	public function pvp($data){
-		$data[0]->sendChat($this->prefix."Пвп началось!");
+	public function pvp(){
+		$players = $this->api->player->getAll();
+		foreach($players as $p){
+			$this->players[$p->username] = $p->username;
+		}
+		//console(var_dump($this->players));
+		$this->api->chat->broadcast($this->prefix."Пвп началось!");
 		$this->status = "pvp";
 	}
 }
