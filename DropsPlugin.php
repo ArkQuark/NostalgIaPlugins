4d849bdadd2d774fe39dbe789c84403dd7a925b6
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2023-07-22 23:14:12 +0300

    DropsPlugin 1.4.0

--- DropsPlugin.php
+++ DropsPlugin.php
@@ -4,15 +4,13 @@
 __PocketMine Plugin__
 name=DropsPlugin
 description=New drops from blocks
-version=1.3b
+version=1.4.0
 author=ArkQuark
-class=Quartz
+class=DPMain
 apiversion=11,12,12.1
 */
 
-class Quartz implements Plugin{
-	private $api, $blockIdAccessor;
-	
+class DPMain implements Plugin{
 	public function __construct(ServerAPI $api, $server = false){
 		$this->api = $api;
 		$this->blockIdAccessor = (new ReflectionClass("Block"))->getProperty("id");
@@ -20,36 +18,30 @@ class Quartz implements Plugin{
 	}
 	
 	public function init(){
-		$this->api->addHandler("player.block.break", array($this, "eventHandler"), 0);
+		$this->api->addHandler("player.block.break", [$this, "eventHandler"]);
 	}
 	
 	public function eventHandler($data, $event){
-		switch ($event){
-			case "player.block.break":
+		$block = $data["target"];
+		$player = $data["player"];
+		$pos = new Position($block->x+.5, $block->y, $block->z+.5, $block->level);
 			
-			$block = $data["target"];
-			$player = $data["player"];
-			$level = $block->level;
-			$pos = new Position($block->x+.5, $block->y, $block->z+.5, $level);
-			
-			if(!($player->getGamemode() == "survival")){
-				break;
+		if(!($player->getGamemode() === "survival")) return;
+
+		if(($block->getID() === SAND) or ($block->getID() === NETHERRACK)){
+			if(mt_rand(1, 20) == 1){
+				$pos->level->setBlock(new Vector3($pos->x, $pos->y, $pos->z), new AirBlock());
+				$item = $this->api->block->fromString("QUARTZ");
+				$this->api->entity->drop($pos, $item);
+				$this->blockIdAccessor->setValue($data["target"], 0);
+				return false;
 			}
-			if(($block->getID() == 12) or ($block->getID() == 87)){//Sand and Netherrack
-				if (mt_rand(1, 20) == 1){
-					$level->setBlock(new Vector3($block->x, $block->y, $block->z), new AirBlock());
-					$item = $this->api->block->fromString("QUARTZ");
-					$this->api->entity->drop($pos, $item);
-					$this->blockIdAccessor->setValue($data["target"], 0);
-					return false;
-				}
-			}
+		}
 				
-			elseif(($block->getID() == 18)){//Leaves
-				if (mt_rand(1, 10) == 1){
-					$item = $this->api->block->fromString("STRING");
-					$this->api->entity->drop($pos, $item);
-				}
+		elseif(($block->getID() === LEAVES)){
+			if(mt_rand(1, 10) == 1){
+				$item = $this->api->block->fromString("STRING");
+				$this->api->entity->drop($pos, $item);
 			}
 		}
 	}
