3f46f6b06ed20232e74beed55ef79786a616131a
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2023-07-22 23:09:20 +0300

    ColorCarpet 1.4.0

--- ColorCarpet.php
+++ ColorCarpet.php
@@ -3,8 +3,8 @@
 /*
 __PocketMine Plugin__
 name=ColorCarpet
-description=Click on carpet with dye and his now dyed
-version=1.3.2
+description=Coloring wool and carpet with dye
+version=1.4.0
 author=ArkQuark
 class=Carpet
 apiversion=11,12,12.1
@@ -29,38 +29,30 @@ class Carpet implements Plugin{
 		$targetID = $target->getID();
 		$targetMeta = $target->getMetadata();
 		
-		if(($targetID != 35) or ($targetID != 171)) break;
-		if($targetMeta > 15) break;
+		if(($targetID !== WOOL) or ($targetID !== CARPET)) return;
 		
 		$pos = new Vector3($target->x, $target->y, $target->z, $target->level);
 		
 		$itemHeld = $player->getSlot($player->slot);
 		$itemHeldID = $itemHeld->getID();
-		$itemHeldCount = $itemHeld->count;
-		$itemHeldReflection = new ReflectionClass('Item');
-		$itemHeldReflectionCount = $itemHeldReflection->getProperty('count');
 		
 		if($itemHeldID == 351){//dye
-			if(($targetMeta ^ 0xf) != $itemHeldMeta){
+			if(($targetMeta ^ 0xf) !== $itemHeldMeta){
 				$block = BlockAPI::get($targetID, $targetMeta ^ 0xf);
 				$target->level->setBlock($pos, $block);
-				if($playerGamemode == "survival"){
-					if($itemHeldCount = 1) $player->removeItem($itemHeldID, $itemHeldMeta , 1);
-					else $itemHeldReflectionCount->setValue($itemHeld, --$itemHeldCount);
+				if($playerGamemode === "survival"){
+					$player->removeItem($itemHeldID, $itemHeldMeta, 1, false);
 				}
 			}
-			break;
 		}
-		elseif($itemHeldID == 263){//coal
-			if($targetMeta != 15){
+		elseif($itemHeldID === COAL){
+			if($targetMeta !== 15){
 				$block = BlockAPI::get($targetID, 15);
 				$target->level->setBlock($pos, $block);
-				if($playerGamemode == "survival"){
-					if($itemHeldCount = 1) $player->removeItem($itemHeldID, $itemHeldMeta , 1);
-					else $itemHeldReflectionCount->setValue($itemHeld, --$itemHeldCount);
+				if($playerGamemode === "survival"){
+					$player->removeItem($itemHeldID, $itemHeldMeta, 1, false);
 				}
 			}
-			break;
 		}
 	}
 
